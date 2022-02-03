5af9e01829327975ad8ab8e1daa2ded32823e376
Jirka1993 <kleric13@seznam.cz>
2022-02-03 19:57:25 +0100

    pagination function

--- pages/2.js
+++ pages/2.js
@@ -9,13 +9,57 @@ fetch("../data.csv")
     // Paragraph arrays
     const paragraph = getParagraph(2000, 2999);
 
-    // Dom manipulation
-    paragraph.forEach((item) => {
-      const tr = document.createElement("tr");
-      const td = item.map((item) => `<td>${item}</td>`).join("");
+    // pagination
+    const list_element = document.getElementById("tbody");
+    const pagination_element = document.getElementById("pagination");
 
-      tr.innerHTML = td;
+    let current_page = 1;
+    let rows = 20;
 
-      document.querySelector("table").appendChild(tr);
-    });
+    function DisplayList(items, wrapper, rows_per_page, page) {
+      wrapper.innerHTML = "";
+      page--;
+
+      let start = rows_per_page * page;
+      let end = start + rows_per_page;
+      let paginatedItems = items.slice(start, end);
+
+      for (let i = 0; i < paginatedItems.length; i++) {
+        let item = paginatedItems[i];
+
+        // Dom manipulation
+        const td = item.map((item) => `<td>${item}</td>`).join("");
+        const tr = document.createElement("tr");
+        tr.innerHTML = td;
+        document.querySelector("tbody").appendChild(tr);
+        wrapper.appendChild(tr);
+      }
+    }
+    // Dom button manipulation
+    function SetupPagination(items, wrapper, rows_per_page) {
+      wrapper.innerHTML = "";
+
+      let page_count = Math.ceil(items.length / rows_per_page);
+      for (let i = 1; i < page_count + 1; i++) {
+        let btn = PaginationButton(i, items);
+        wrapper.appendChild(btn);
+      }
+    }
+    //  button click on page manipulation
+    function PaginationButton(page, items) {
+      let button = document.createElement("button");
+      button.innerText = page;
+
+      if (current_page == page) button.classList.add("active");
+
+      button.addEventListener("click", function () {
+        current_page = page;
+        DisplayList(items, list_element, rows, current_page);
+      });
+
+      return button;
+    }
+
+    DisplayList(paragraph, list_element, rows, current_page);
+    SetupPagination(paragraph, pagination_element, rows);
   });
